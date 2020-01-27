a34c664bd6f624c648686fcd2036e5661df7cb87
callmenick <nick@callmenick.com>
2020-01-27 16:39:09 -0400

    updates gulpfile to use new version syntax

--- gulpfile.js
+++ gulpfile.js
@@ -1,34 +1,24 @@
-'use strict';
+const { src, dest, watch, series, parallel } = require('gulp');
+const autoprefixer = require('autoprefixer');
+const cssnano = require('cssnano');
+const postcss = require('gulp-postcss');
+const sass = require('gulp-sass');
 
-var gulp = require('gulp');
-var autoprefixer = require('autoprefixer');
-var cssnano = require('cssnano');
-var postcss = require('gulp-postcss');
-var sass = require('gulp-sass');
-
-
-/* styles tasks */
-
-gulp.task('styles', function () {
-  return gulp.src('./src/sass/**/*.scss')
+function styles() {
+  return src('./src/sass/**/*.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(postcss([
       autoprefixer(),
       cssnano()
     ]))
-    .pipe(gulp.dest('./themes/callmenick/static/css'));
-});
+    .pipe(dest('./themes/callmenick/static/css'));
+}
 
-/* dev tasks */
+function dev() {
+  watch(['./src/sass/**/*.scss'], {}, parallel(styles));
+}
 
-gulp.task('watch', ['styles'], function() {
-  gulp.watch('./src/sass/**/*.scss', ['styles']);
-});
-
-/* build tasks */
-
-gulp.task('build', ['styles']);
-
-/* default tasks */
-
-gulp.task('default', ['build']);
+exports.styles = styles;
+exports.dev = dev;
+exports.build = parallel(styles);
+exports.default = parallel(styles);
